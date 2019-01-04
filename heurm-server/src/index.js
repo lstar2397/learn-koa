5e4651170f6722404b3c2fbc111c9cef24055a33
Starlet <lstar2397@naver.com>
2019-01-05 08:20:34 +0900

    Update index.js

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -1,12 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use((ctx, next) => {
+app.use(async (ctx, next) => {
     console.log(1);
     const started = new Date();
-    next().then(() => {
-        console.log(new Date() - started + 'ms');
-    });
+    await next();
+    console.log(new Date() - started + 'ms');
 });
 
 app.use((ctx, next) => {
