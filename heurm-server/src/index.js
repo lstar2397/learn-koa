b4caa4ad76c87ce7ae5a8483290dbb28991f2117
Starlet <lstar2397@naver.com>
2019-01-05 08:18:58 +0900

    Update index.js

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -1,6 +1,16 @@
 const Koa = require('koa');
 const app = new Koa();
 
+app.use((ctx, next) => {
+    console.log(1);
+    next();
+});
+
+app.use((ctx, next) => {
+    console.log(2);
+    next();
+});
+
 app.use(ctx => {
     ctx.body = 'Hello, Koa!';
 });
