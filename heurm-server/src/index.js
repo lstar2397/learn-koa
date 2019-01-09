a9862ef5f884d58656110abdc246331f04e129cf
Starlet <lstar2397@naver.com>
2019-01-09 10:28:05 +0900

    3-4

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -11,17 +11,23 @@ const mongoose = require('mongoose');
 const bodyParser = require('koa-bodyparser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI).then((res) => {
-    console.log('Successfully connected to mongodb');
-}).catch(e => {
-    console.error(e);
-});
+mongoose
+    .connect(
+        process.env.MONGO_URI,
+        { useNewUrlParser: true }
+    )
+    .then(res => {
+        console.log('Successfully connected to mongodb');
+    })
+    .catch(e => {
+        console.error(e);
+    });
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser());
 router.use('/api', api.routes());
-    
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
